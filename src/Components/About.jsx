faaf1ee759e030ae4e328534bddbbb744e10bd5c
Rishav Kumar <rishavkumar8507@gmail.com>
2025-06-17 23:26:08 +0530

    clear

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -22,45 +22,21 @@ const About = () => {
     },
   });
 
-//   const el = useRef(null);
 
-//   useEffect(() => {
-//     const typed = new Typed(el.current, {
-//       strings: ["Java Developer.", "Backend Developer.", "Frontend Developer."],
-//       startDelay: 100,
-//       typeSpeed: 50,
-//       backSpeed: 10,
-//       backDelay: 100,
-//       loop: true,
-//     });
-
-//     return () => {
-//       typed.destroy();
-//     };
-//   }, []);
 
   return (
     <div className="main-container border py-20 bg-gray-200 px-4">
       <h1 className="text-center text-4xl pb-6 font-bold">About</h1>
       <h2 className="text-center pb-16">{data.title}</h2>
-      {/* <h2 className="text-xl text-center pb-16">{data.title}</h2> */}
+      
       <div className="inside-main flex flex-col md:flex-row items-center gap-12 max-w-6xl mx-auto">
         
-        {/* Image
-        <div className="w-full md:w-1/2 flex justify-center">
-          <div className="w-2/3 md:w-full">
-             <img className="rounded-full w-full" src={data.image} alt="Rishav Kumar" /> 
-             
-          </div>
-        </div> */}
+        
 
         {/* Text Content */}
         <div className="w-full md:w-full flex justify-center text-center md:text-left">
           <div className="space-y-2 text-center">
-            {/* <h2 className="text-3xl">
-              I'm Rishav and I'm{" "}
-              <span className="font-bold underline text-orange-500" ref={el}></span>
-            </h2> */}
+            
             
             <h1 className="text-4xl underline pb-6">Benifits </h1>
             <span className="font-bold">{data.title1}</span><span>{data.desc1}</span> <br />
@@ -69,14 +45,7 @@ const About = () => {
             <span className="font-bold">{data.title4}</span><span>{data.desc4}</span> <br />
             <span className="font-bold">{data.title5}</span><span>{data.desc5}</span> <br />
             
-            {/* <a
-              href={data.actionButton.link}
-              className="inline-block mt-3 px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white rounded-full shadow-lg"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {data.actionButton.title}
-            </a> */}
+           
           </div>
         </div>
       </div>
